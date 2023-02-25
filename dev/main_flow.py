113af9c4c5d45139dfa32a9eebe9005f9d8640fc
Im-Gonzo <gonzaloalcala0304@gmail.com>
2023-02-25 13:28:23 -0600

    add main flow structure for pipeline

---
+++ dev/main_flow.py
@@ -0,0 +1,10 @@
+from prefect import flow, task
+
+@flow()
+def main_audiomatic_flow():
+    """Main tasks for video transcribtion and translation"""
+    pass
+
+
+if __name__ == '__main__':
+    pass
